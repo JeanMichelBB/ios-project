965b6e6d6583c69f690d277904f6db5842918959
Jean-Michel BB <jeanmichelbberube@outlook.com>
2023-03-28 09:21:14 -0400

    Initial commit

---
+++ pokedex-ios/pokedex-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex-ios
+//
+//  Created by Jean-Michel Beaulieu Bérubé on 2023-03-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
