3bae198618019e6c03e7a281a2b67163828680a6
Jean-Michel BB <jeanmichelbberube@outlook.com>
2023-04-02 09:30:57 -0400

    basic

---
+++ pokedex-ios/pokedex-ios/SelectedCellViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectedCellViewController.swift
+//  pokedex-ios
+//
+//  Created by Jean-Michel Beaulieu Bérubé on 2023-04-02.
+//
+
+import UIKit
+
+class SelectedCellViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
