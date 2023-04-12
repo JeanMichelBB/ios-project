1a033fce89438e8652944049ca4ff78bcbe1ac00
anyruizd <anyruizd@gmail.com>
2023-04-11 20:17:36 -0400

    add pagination and cache

--- pokedex-ios/pokedex-ios/View/PokemonTableViewCell.swift
+++ pokedex-ios/pokedex-ios/View/PokemonTableViewCell.swift
@@ -22,7 +22,6 @@ class PokemonTableViewCell: UITableViewCell {
         
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -65,41 +64,41 @@ class PokemonTableViewCell: UITableViewCell {
     func getLabelColor(label : String) -> UIColor {
         switch label {
             case "normal":
-                return UIColor(red: 0.78, green: 0.78, blue: 0.60, alpha: 1.00)
+                return UIColor(red: 0.66, green: 0.65, blue: 0.48, alpha: 1.00)
             case "fighting":
-                return UIColor(red: 0.78, green: 0.44, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.76, green: 0.18, blue: 0.16, alpha: 1.00)
             case "flying":
-                return UIColor(red: 0.60, green: 0.60, blue: 0.92, alpha: 1.00)
+                return UIColor(red: 0.66, green: 0.56, blue: 0.95, alpha: 1.00)
             case "poison":
-                return UIColor(red: 0.60, green: 0.44, blue: 0.60, alpha: 1.00)
+                return UIColor(red: 0.64, green: 0.24, blue: 0.63, alpha: 1.00)
             case "ground":
-                return UIColor(red: 0.92, green: 0.76, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.89, green: 0.75, blue: 0.40, alpha: 1.00)
             case "rock":
-                return UIColor(red: 0.60, green: 0.44, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.71, green: 0.63, blue: 0.21, alpha: 1.00)
             case "bug":
-                return UIColor(red: 0.60, green: 0.76, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.65, green: 0.73, blue: 0.10, alpha: 1.00)
             case "ghost":
-                return UIColor(red: 0.44, green: 0.44, blue: 0.60, alpha: 1.00)
+                return UIColor(red: 0.45, green: 0.34, blue: 0.59, alpha: 1.00)
             case "steel":
-                return UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.00)
+                return UIColor(red: 0.72, green: 0.72, blue: 0.81, alpha: 1.00)
             case "fire":
-                return UIColor(red: 0.92, green: 0.44, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.93, green: 0.51, blue: 0.19, alpha: 1.00)
             case "water":
-                return UIColor(red: 0.44, green: 0.60, blue: 0.92, alpha: 1.00)
+                return UIColor(red: 0.39, green: 0.56, blue: 0.94, alpha: 1.00)
             case "grass":
-                return UIColor(red: 0.44, green: 0.92, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.48, green: 0.78, blue: 0.30, alpha: 1.00)
             case "electric":
-                return UIColor(red: 0.92, green: 0.92, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.97, green: 0.82, blue: 0.17, alpha: 1.00)
             case "psychic":
-                return UIColor(red: 0.92, green: 0.44, blue: 0.92, alpha: 1.00)
+                return UIColor(red: 0.98, green: 0.33, blue: 0.53, alpha: 1.00)
             case "ice":
-                return UIColor(red: 0.44, green: 0.92, blue: 0.92, alpha: 1.00)
+                return UIColor(red: 0.59, green: 0.85, blue: 0.84, alpha: 1.00)
             case "dragon":
-                return UIColor(red: 0.44, green: 0.44, blue: 0.92, alpha: 1.00)
+                return UIColor(red: 0.44, green: 0.21, blue: 0.99, alpha: 1.00)
             case "dark":
-                return UIColor(red: 0.44, green: 0.44, blue: 0.44, alpha: 1.00)
+                return UIColor(red: 0.44, green: 0.34, blue: 0.27, alpha: 1.00)
             case "fairy":
-                return UIColor(red: 0.92, green: 0.44, blue: 0.60, alpha: 1.00)
+                return UIColor(red: 0.84, green: 0.52, blue: 0.68, alpha: 1.00)
             default:
                 return UIColor(red: 0.78, green: 0.78, blue: 0.60, alpha: 1.00)
         }
